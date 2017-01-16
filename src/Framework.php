a041e515aead56c8843e47be951d200f9d632645
Juergen Eger <juergen.eger@wirecard.com>
2017-01-16 13:50:07 +0100

    Setup of initial project structure

---
+++ src/Framework.php
@@ -0,0 +1,6 @@
+<?php
+namespace Wirecard\PaymentSdk;
+
+class Framework
+{
+}
---
+++ test/WirecardTest/PaymentSdk/FrameworkUTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace WirecardTest\PaymentSdk;
+
+class FrameworkUTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFramework()
+    {
+        $this->assertTrue(true);
+    }
+}
