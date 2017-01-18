b2b9f2ef835b5dd08b1248d241dab6b4964a86da
Juergen Eger <juergen.eger@wirecard.com>
2017-01-18 16:09:27 +0100

    testing coverage and sonarqube report

--- src/Framework.php
+++ src/Framework.php
@@ -3,4 +3,8 @@ namespace Wirecard\PaymentSdk;
 
 class Framework
 {
+    function hello($world) {
+        return 'Hello ' . $world;
+    }
 }
+?>
--- test/FrameworkUTest.php
+++ test/FrameworkUTest.php
@@ -1,10 +1,21 @@
 <?php
 namespace WirecardTest\PaymentSdk;
 
+use Wirecard\PaymentSdk\Framework;
+
 class FrameworkUTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Framework
+     */
+    protected $framework;
+
+    public function setUp() {
+        $this->framework = new Framework();
+    }
+
     public function testFramework()
     {
-        $this->assertTrue(true);
+        $this->assertEquals('Hello world', $this->framework->hello('world'));
     }
 }
