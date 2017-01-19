05402e4cb2714bec22ec370b2831e43b023fe7b6
Timon Rönisch <timon.roenisch@wirecard.com>
2017-01-19 16:34:49 +0100

    Update documentation

--- src/Framework.php
+++ src/Framework.php
@@ -1,8 +1,22 @@
 <?php
 namespace Wirecard\PaymentSdk;
 
+
+/**
+ * Class Framework
+ *
+ * Framework class for basic structure
+ *
+ * @package Wirecard\PaymentSdk
+ */
 class Framework
 {
+    /**
+     * Return the given world string
+     *
+     * @param $world
+     * @return string
+     */
     public function hello($world)
     {
         return 'Hello ' . $world;
