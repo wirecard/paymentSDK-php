082a162d7a57daea27bb0d33f6fffaff59dc815d
reka <reka.horvath85@gmail.com>
2017-01-25 15:52:56 +0100

    added unit test to PayPalTransaction

---
+++ test/PayPalTransactionUTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WirecardTest\PaymentSdk;
+
+use Wirecard\PaymentSdk\PayPalTransaction;
+
+class PayPalTransactionUTest extends \PHPUnit_Framework_TestCase
+{
+    const AMOUNT = 42.21;
+    const EUR = 'EUR';
+    const NOTIFICATION_URL = 'http://www.example.com';
+
+    /**
+     * @var PayPalTransaction
+     */
+    private $payPalTransaction;
+
+    public function setUp()
+    {
+        $this->payPalTransaction = new PayPalTransaction(self::AMOUNT, self::EUR, self::NOTIFICATION_URL);
+    }
+
+    public function testGetAmount()
+    {
+        $this->assertEquals(self::AMOUNT, $this->payPalTransaction->getAmount());
+    }
+
+    public function testGetCurrency()
+    {
+        $this->assertEquals(self::EUR, $this->payPalTransaction->getCurrency());
+    }
+
+    public function testGetNotificationUrl()
+    {
+        $this->assertEquals(self::NOTIFICATION_URL, $this->payPalTransaction->getNotificationUrl());
+    }
+}
+
+   
