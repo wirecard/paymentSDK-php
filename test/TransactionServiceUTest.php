d6abbbc7718fb903e1c32388105a9c3dcbd3b0f1
Horst Fickel <horst.fickel@wirecard.com>
2017-01-26 13:04:29 +0100

    #3 changed testing of protected methods

--- test/TransactionServiceUTest.php
+++ test/TransactionServiceUTest.php
@@ -8,7 +8,7 @@ use Wirecard\PaymentSdk\TransactionService;
 class TransactionServiceUTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var TransactionServicePublic
+     * @var TransactionService
      */
     private $instance;
 
@@ -20,13 +20,13 @@ class TransactionServiceUTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->config = $this->createMock('\Wirecard\PaymentSdk\Config');
-        $this->instance = new TransactionServicePublic($this->config);
+        $this->instance = new TransactionService($this->config);
     }
 
     public function testFullConstructor()
     {
-        $logger = $this->createMock('Monolog\Logger');
-        $httpClient = $this->createMock('GuzzleHttp\Client');
+        $logger = $this->createMock('\Monolog\Logger');
+        $httpClient = $this->createMock('\GuzzleHttp\Client');
         $instance = new TransactionService($this->config, $logger, $httpClient);
 
         $this->assertAttributeEquals($this->config, 'config', $instance);
@@ -36,33 +36,34 @@ class TransactionServiceUTest extends \PHPUnit_Framework_TestCase
 
     public function testGetConfig()
     {
-        $this->assertEquals($this->config, $this->instance->getConfig());
+        $helper = function () {
+            return $this->getConfig();
+        };
+
+        $method = $helper->bindTo($this->instance, $this->instance);
+
+        $this->assertEquals($this->config, $method());
     }
+
     public function testGetLogger()
     {
-        $this->assertInstanceOf('\Psr\Log\LoggerInterface', $this->instance->getLogger());
+        $helper = function () {
+            return $this->getLogger();
+        };
+
+        $method = $helper->bindTo($this->instance, $this->instance);
+
+        $this->assertInstanceOf('\Psr\Log\LoggerInterface', $method());
     }
 
     public function testGetHttpClient()
     {
-        $this->assertInstanceOf('GuzzleHttp\Client', $this->instance->getHttpClient());
-    }
-}
+        $helper = function () {
+            return $this->getHttpClient();
+        };
 
-class TransactionServicePublic extends TransactionService
-{
-    public function getLogger()
-    {
-        return parent::getLogger();
-    }
+        $method = $helper->bindTo($this->instance, $this->instance);
 
-    public function getConfig()
-    {
-        return parent::getConfig();
-    }
-
-    public function getHttpClient()
-    {
-        return parent::getHttpClient();
+        $this->assertInstanceOf('\GuzzleHttp\Client', $method());
     }
 }
