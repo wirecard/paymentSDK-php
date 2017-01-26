59438cd3e46c96c6d00d63d0c83602a6c8a8a17a
reka <reka.horvath85@gmail.com>
2017-01-26 18:08:02 +0100

    added RequestMapper and RequestIdGenerator

---
+++ src/RequestMapper.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Wirecard\PaymentSdk;
+
+class RequestMapper
+{
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @var RequestIdGenerator
+     */
+    private $requestIdGenerator;
+
+    /**
+     * RequestMapper constructor.
+     * @param Config $config
+     * @param RequestIdGenerator $requestIdGenerator
+     */
+    public function __construct(Config $config, RequestIdGenerator $requestIdGenerator)
+    {
+        $this->config = $config;
+        $this->requestIdGenerator = $requestIdGenerator;
+    }
+
+    /**
+     * @param PayPalTransaction $transaction
+     * @return The transaction in JSON format.
+     */
+    public function map(PayPalTransaction $transaction)
+    {
+        $onlyPaymentMethod = ['payment-method' => ['name' => 'paypal']];
+        $amount = [
+            'currency' => $transaction->getAmount()->getCurrency(),
+            'value' => $transaction->getAmount()->getAmount()
+        ];
+        $requestId = $this->requestIdGenerator->generate();
+
+        $result = ['payment' => [
+            'merchant-account-id' => $this->config->getMerchantAccountId(),
+            'request-id' => $requestId,
+            'transaction-type' => 'debit',
+            'payment-methods' => [$onlyPaymentMethod],
+            'requested-amount' => $amount
+        ]];
+        return json_encode($result);
+    }
+}
---
+++ test/RequestMapperUTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace WirecardTest\PaymentSdk;
+
+use Wirecard\PaymentSdk\Config;
+use Wirecard\PaymentSdk\Money;
+use Wirecard\PaymentSdk\PayPalTransaction;
+use Wirecard\PaymentSdk\RequestIdGenerator;
+use Wirecard\PaymentSdk\RequestMapper;
+
+class RequestMapperUTest extends \PHPUnit_Framework_TestCase
+{
+    const MAID = 'B612';
+    /**
+     * @var RequestMapper
+     */
+    private $mapper;
+
+    /**
+     * @var RequestIdGenerator
+     */
+    private $requestIdGeneratorMock;
+
+    public function setUp()
+    {
+        $config = new Config('dummyUser', 'dummyPassword', self::MAID, 'secret');
+        $this->requestIdGeneratorMock = $this->createMock('Wirecard\PaymentSdk\RequestIdGenerator');
+        $this->mapper = new RequestMapper($config, $this->requestIdGeneratorMock);
+    }
+
+    public function testSample()
+    {
+        $this->requestIdGeneratorMock->method('generate')
+            ->willReturn('5B-dummy-id');
+
+        $expectedResult = ['payment' => [
+            "merchant-account-id" => "B612",
+        "request-id" => "5B-dummy-id",
+            "transaction-type" => "debit",
+            "payment-methods" => [["payment-method"=>["name"=>"paypal"]]],
+            "requested-amount"=>["currency"=>"EUR","value"=>24]
+        ]];
+
+        $transaction = new PayPalTransaction(new Money(24, 'EUR'), 'http://www.example.com');
+        $result = $this->mapper->map($transaction);
+
+        $this->assertEquals(json_encode($expectedResult), $result);
+
+        /*
+         * expected result (as JSON):
+        {
+        "payment":
+        {"merchant-account-id":"B612",
+        "request-id":"5B-dummy-id","
+        transaction-type":"debit",
+        "payment-methods":[{"payment-method":{"name":"paypal"}}],
+        "requested-amount":{"currency":"EUR","value":24}
+        }}
+        */
+    }
+}
