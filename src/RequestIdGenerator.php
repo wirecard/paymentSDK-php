4f6bb8fb3e5b6735481b02ef6141d83ad5ab6efb
reka <reka.horvath85@gmail.com>
2017-01-27 09:18:15 +0100

    generating request ID: uniqid => openssl_random_pseudo_byte

--- src/RequestIdGenerator.php
+++ src/RequestIdGenerator.php
@@ -5,10 +5,11 @@ namespace Wirecard\PaymentSdk;
 class RequestIdGenerator
 {
     /**
-     * @return string A random, unique ID.
+     * @param int $length The length of the generated ID.
+     * @return string A random, unique ID for the request.
      */
-    public function generate()
+    public function generate($length = 32)
     {
-        return uniqid('', false);
+        return substr(bin2hex(openssl_random_pseudo_bytes($length)), 0, $length);
     }
 }
