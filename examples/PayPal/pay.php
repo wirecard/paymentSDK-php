c54230359e735132a722805a33bc894f967adce3
Juergen Eger <juergen.eger@wirecard.com>
2017-01-30 10:15:23 +0100

    #3 adding PayPal return to example

--- examples/PayPal/pay.php
+++ examples/PayPal/pay.php
@@ -3,8 +3,10 @@
 // # PayPal payment transaction
 // This example displays the usage payments for payment method PayPal
 
+// we are using psr-4 autoloading through composer
 require __DIR__ . '/../../vendor/autoload.php';
 
+use Wirecard\PaymentSdk\Config;
 use Wirecard\PaymentSdk\FailureResponse;
 use Wirecard\PaymentSdk\InteractionResponse;
 use Wirecard\PaymentSdk\Money;
@@ -12,13 +14,30 @@ use Wirecard\PaymentSdk\PayPalTransaction;
 use Wirecard\PaymentSdk\Redirect;
 use Wirecard\PaymentSdk\TransactionService;
 
+/**
+ * @param $path
+ * @return string
+ */
+function getUrl($path)
+{
+    $protocol = 'http';
+
+    if ($_SERVER['SERVER_PORT'] == 443 || (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) === 'on')) {
+        $protocol .= 's';
+    }
+
+    $host = $_SERVER['HTTP_HOST'];
+    $request = $_SERVER['PHP_SELF'];
+    return dirname(sprintf('%s://%s%s', $protocol, $host, $request)) . '/' . $path;
+}
+
 // ### Money object
 // Use the money object as amount which has to be payed by the consumer
 $amount = new Money(12.59, 'EUR');
 
 // ### Redirect urls
 // The redirect urls determine where the consumer should be redirected by PayPal after approval/cancellation.
-$redirectUrls = new Redirect('https://www.example.com/paypal/return/success', 'https://www.example.com/paypal/return/cancel');
+$redirectUrls = new Redirect(getUrl('return.php?status=success'), getUrl('return.php?status=cancel'));
 
 // ### Notification url
 // As soon as the transaction status changes a server-to-server notification will get delivered to this url
@@ -30,7 +49,7 @@ $transaction = new PayPalTransaction($amount, $notificationUrl, $redirectUrls);
 
 // ### Config
 // The config object holds all interface configuration options
-$config = new Config('70000-APITEST-AP', 'qD2wzQ_hrc!8', '9abf05c1-c266-46ae-8eac-7f87ca97af28', '5fca2a83-89ca-4f9e-8cf7-4ca74a02773f');
+$config = new Config('https://api-test.wirecard.com/engine/rest/paymentmethods/', '70000-APITEST-AP', 'qD2wzQ_hrc!8', '9abf05c1-c266-46ae-8eac-7f87ca97af28', '5fca2a83-89ca-4f9e-8cf7-4ca74a02773f');
 
 // ### Transaction Service
 // The service is used to execute the payment operation itself. A response object is returned.
@@ -46,12 +65,15 @@ if ($response instanceof InteractionResponse) {
     exit;
 // The failure state is represented by a FailureResponse object. In this case the returned errors should be stored in your system
 } else if ($response instanceof FailureResponse) {
-// In our simple example we iterate over all errors and echo them out
+// In our example we iterate over all errors and echo them out. You should display them as error, warning or information based on the given severity
     foreach ($response->getStatusCollection() AS $status) {
+        /**
+         * @var $status \Wirecard\PaymentSdk\Status
+         */
         $severity = ucfirst($status->getSeverity());
         $code = $status->getCode();
         $description = $status->getDescription();
-        echo sprintf('%s with code %s and message %s occured.<br>', $severity, $code, $description);
+        echo sprintf('%s with code %s and message "%s" occured.<br>', $severity, $code, $description);
     }
 }
 
---
+++ examples/PayPal/return.php
@@ -0,0 +1,6 @@
+<?php
+// # PayPal return after transaction
+// The consumer gets redirected to this page after a paypal transaction
+
+// we are using psr-4 autoloading through composer
+require __DIR__ . '/../../vendor/autoload.php';
