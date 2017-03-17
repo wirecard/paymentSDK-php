1618a60d167071eb4e40ca469c047721d0e0ab79
Horst Fickel <horst.fickel@wirecard.com>
2017-03-17 10:31:14 +0100

    sofort example for referenced pay transaction done with sepa

--- src/Transaction/SofortTransaction.php
+++ src/Transaction/SofortTransaction.php
@@ -83,26 +83,6 @@ class SofortTransaction extends Transaction
      * @throws UnsupportedOperationException
      * @return string
      */
-    public function getConfigKey()
-    {
-        if (null !== $this->parentTransactionId) {
-            switch ($this->operation) {
-                case Operation::PAY:
-                    return SepaTransaction::DIRECT_DEBIT;
-                case Operation::CREDIT:
-                    return SepaTransaction::CREDIT_TRANSFER;
-                default:
-                    throw new UnsupportedOperationException();
-            }
-        }
-
-        return parent::getConfigKey();
-    }
-
-    /**
-     * @throws UnsupportedOperationException
-     * @return string
-     */
     private function retrieveTransactionType()
     {
         $transactionTypes = [
