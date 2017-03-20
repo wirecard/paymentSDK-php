ecdb11e949cb8410c7881cdfd39029ced5d7e7e2
Horst Fickel <horst.fickel@wirecard.com>
2017-03-20 14:42:47 +0100

    changed money back to original value

--- examples/PayPal/reserve.php
+++ examples/PayPal/reserve.php
@@ -35,7 +35,7 @@ $config->add($paypalConfig);
 
 // ### Transaction related objects
 // Use the money object as amount which has to be payed by the consumer.
-$amount = new Money(0.0, 'EUR');
+$amount = new Money(12.59, 'EUR');
 
 // If there was a previous transaction, use the ID of this parent transaction as reference.
 $parentTransactionId = array_key_exists('parentTransactionId', $_POST) ? $_POST['parentTransactionId'] : null;
