0747480a2f299c7c92d2785c79b01fa69de4e751
Timon Roenisch <timon.roenisch@wirecard.com>
2017-03-20 14:03:38 +0100

    Add items to PayPal reserve

--- examples/PayPal/reserve.php
+++ examples/PayPal/reserve.php
@@ -8,11 +8,11 @@ require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../inc/common.php';
 
 use Wirecard\PaymentSdk\Config;
+use Wirecard\PaymentSdk\Entity\Money;
+use Wirecard\PaymentSdk\Entity\Redirect;
 use Wirecard\PaymentSdk\Response\FailureResponse;
 use Wirecard\PaymentSdk\Response\InteractionResponse;
-use Wirecard\PaymentSdk\Entity\Money;
 use Wirecard\PaymentSdk\Transaction\PayPalTransaction;
-use Wirecard\PaymentSdk\Entity\Redirect;
 use Wirecard\PaymentSdk\TransactionService;
 
 // ### Config
@@ -71,6 +71,7 @@ $tx->setNotificationUrl($notificationUrl);
 $tx->setRedirect($redirectUrls);
 $tx->setAmount($amount);
 $tx->setParentTransactionId($parentTransactionId);
+$tx->setItemCollection($itemCollection);
 
 // ### Transaction Service
 // The service is used to execute the reserve operation itself. A response object is returned.
