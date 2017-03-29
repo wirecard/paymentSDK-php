840d4c118bf51a426e89889a4ae76c377185d383
Timon Roenisch <timon.roenisch@wirecard.com>
2017-03-29 09:26:46 +0200

    Change directory

--- .bin/generate-tag.sh
+++ .bin/generate-tag.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-cd $HOME/wirecard/paymentSDK-php/
+pwd
+
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 
 TAG=`git describe --tags --abbrev=0`
 VERSION=`cat VERSION`
@@ -10,6 +13,19 @@ echo "Tag and version information:"
 echo "Latest tag: $TAG"
 echo "Version set: $VERSION"
 
-if [[ $TAG == $VERSION ]] ; then
+if [[ ${TAG} == ${VERSION} ]] ; then
     echo "Tag and version are different!"
 fi
+
+git config user.name "Travis CI"
+git config user.email "wirecard@travis-ci.org"
+
+# git commit -m "Create release tag for version $VERSION"
+
+openssl aes-256-cbc -K ${encrypted_5b57bcef90c0_key} -iv ${encrypted_5b57bcef90c0_iv} -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+# git push ${SSH_REPO} master
