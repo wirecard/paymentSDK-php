6a3bc3d617782d1a8825049d6dd9f43cb59b49df
Timon Roenisch <timon.roenisch@wirecard.com>
2017-03-29 09:19:12 +0200

    Add debug info

--- .bin/generate-tag.sh
+++ .bin/generate-tag.sh
@@ -6,3 +6,10 @@ cd $HOME/wirecard/paymentSDK-php/
 TAG=`git describe --tags --abbrev=0`
 VERSION=`cat VERSION`
 
+echo "Tag and version information:"
+echo "Latest tag: $TAG"
+echo "Version set: $VERSION"
+
+if [[ $TAG == $VERSION ]] ; then
+    echo "Tag and version are different!"
+fi
