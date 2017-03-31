13db48d6b3aa8b7c13641bc403c8ce37d8cbb8b4
Timon Roenisch <timon.roenisch@wirecard.com>
2017-03-31 13:08:18 +0200

    Use config file in RatePAY invoice examples

--- examples/RatePAY_Invoice/credit.php
+++ examples/RatePAY_Invoice/credit.php
@@ -1,42 +1,23 @@
 <?php
 // # Credit via RatePAY
+
 // To transfer funds via a credit operation, information on the receiver are required.
 // A request is sent with the account holder information.
 
 // ## Required objects
+
 // To include the necessary files, use the composer for PSR-4 autoloading.
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../inc/common.php';
+require __DIR__ . '/../inc/config.php';
 
-use Wirecard\PaymentSdk\Config;
 use Wirecard\PaymentSdk\Entity\Amount;
 use Wirecard\PaymentSdk\Response\FailureResponse;
 use Wirecard\PaymentSdk\Response\SuccessResponse;
 use Wirecard\PaymentSdk\Transaction\RatepayInvoiceTransaction;
 use Wirecard\PaymentSdk\TransactionService;
 
-// ### Config
-// #### Basic configuration
-// The basic configuration requires the base URL for Wirecard and the username and password for the HTTP requests.
-$baseUrl = 'https://api-test.wirecard.com';
-$httpUser = '70000-APITEST-AP';
-$httpPass = 'qD2wzQ_hrc!8';
-
-// The configuration is stored in an object containing the connection settings set above.
-// A default currency can also be provided.
-$config = new Config\Config($baseUrl, $httpUser, $httpPass, 'EUR');
-
-// #### Config for RatePAY
-// Create and add a configuration object with the RatePAY settings
-$ratepayMAID = '9abf05c1-c266-46ae-8eac-7f87ca97af28';
-$ratepayKey = '5fca2a83-89ca-4f9e-8cf7-4ca74a02773f';
-
-$ratepayConfig = new Config\PaymentMethodConfig(
-    RatepayInvoiceTransaction::NAME,
-    $ratepayMAID,
-    $ratepayKey
-);
-$config->add($ratepayConfig);
+// ### Transaction related objects
 
 // Use the amount object as amount which has to be payed by the consumer.
 if (array_key_exists('amount', $_POST)) {
@@ -47,6 +28,7 @@ if (array_key_exists('amount', $_POST)) {
 $amount = new Amount($amountValue, 'EUR');
 
 // ### Notification URL
+
 // As soon as the transaction status changes, a server-to-server notification will get delivered to this URL.
 $notificationUrl = getUrl('notify.php');
 
@@ -67,11 +49,12 @@ $transaction->setAmount($amount);
 $transaction->setParentTransactionId($_POST['parentTransactionId']);
 $transaction->setItemCollection($itemCollection);
 
-
 // ### Transaction Service
+
 // The service is used to execute the payment operation itself. A response object is returned.
 $transactionService = new TransactionService($config);
 $response = $transactionService->credit($transaction);
+
 
 // ## Response handling
 
