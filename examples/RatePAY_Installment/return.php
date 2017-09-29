4866616b9f2f1685fcbdbb678e9494a36e35ca44
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2017-09-29 10:47:38 +0200

    #189 Add cancel case to return

--- examples/RatePAY_Installment/return.php
+++ examples/RatePAY_Installment/return.php
@@ -27,51 +27,57 @@ $config->setPublicKey(file_get_contents(__DIR__ . '/../inc/api-test.wirecard.com
 
 // The `TransactionService` is used to determine the response from the service provider.
 $service = new TransactionService($config);
-$response = $service->handleResponse($_POST);
+// If there is response data from the service provider handle response
+if ($_POST) {
+    $response = $service->handleResponse($_POST);
 
 
 // ## Payment results
 
 // The response from the service can be used for disambiguation.
 // In case of a successful transaction, a `SuccessResponse` object is returned.
-if ($response instanceof SuccessResponse) {
-    $xmlResponse = new SimpleXMLElement($response->getRawData());
-    $transactionType = $response->getTransactionType();
-    echo 'Reservation successfully completed.<br>';
-    echo sprintf('Response validation status: %s <br>', $response->isValidSignature() ? 'true' : 'false');
-    echo getTransactionLink($baseUrl, $response);
-    ?>
-    <form action="pay-based-on-reserve.php" method="post">
-        <input type="hidden" name="parentTransactionId" value="<?= $response->getTransactionId() ?>"/>
-        <input type="submit" value="Capture the reservation">
-    </form>
-    <br>
-    <form action="cancel.php" method="post">
-        <input type="hidden" name="parentTransactionId" value="<?= $response->getTransactionId() ?>"/>
-        <label for="amount">Amount to cancel:</label>
-        <input type="text" name="amount" id="amount" value="2400"/>
-        <input type="submit" value="Cancel">
-    </form>
-    <form action="credit.php" method="post">
-        <input type="hidden" name="parentTransactionId" value="<?= $response->getTransactionId() ?>"/>
-        <label for="amount">Amount to credit:</label>
-        <input type="text" name="amount" id="amount" value="100"/>
-        <input type="submit" value="Credit">
-    </form>
-    <?php
+    if ($response instanceof SuccessResponse) {
+        $xmlResponse = new SimpleXMLElement($response->getRawData());
+        $transactionType = $response->getTransactionType();
+        echo 'Reservation successfully completed.<br>';
+        echo sprintf('Response validation status: %s <br>', $response->isValidSignature() ? 'true' : 'false');
+        echo getTransactionLink($baseUrl, $response);
+        ?>
+        <form action="pay-based-on-reserve.php" method="post">
+            <input type="hidden" name="parentTransactionId" value="<?= $response->getTransactionId() ?>"/>
+            <input type="submit" value="Capture the reservation">
+        </form>
+        <br>
+        <form action="cancel.php" method="post">
+            <input type="hidden" name="parentTransactionId" value="<?= $response->getTransactionId() ?>"/>
+            <label for="amount">Amount to cancel:</label>
+            <input type="text" name="amount" id="amount" value="2400"/>
+            <input type="submit" value="Cancel">
+        </form>
+        <form action="credit.php" method="post">
+            <input type="hidden" name="parentTransactionId" value="<?= $response->getTransactionId() ?>"/>
+            <label for="amount">Amount to credit:</label>
+            <input type="text" name="amount" id="amount" value="100"/>
+            <input type="submit" value="Credit">
+        </form>
+        <?php
 // In case of a failed transaction, a `FailureResponse` object is returned.
-} elseif ($response instanceof FailureResponse) {
-    echo sprintf('Response validation status: %s <br>', $response->isValidSignature() ? 'true' : 'false');
+    } elseif ($response instanceof FailureResponse) {
+        echo sprintf('Response validation status: %s <br>', $response->isValidSignature() ? 'true' : 'false');
 
-    // In our example we iterate over all errors and echo them out.
-    // You should display them as error, warning or information based on the given severity.
-    foreach ($response->getStatusCollection() as $status) {
-        /**
-         * @var $status \Wirecard\PaymentSdk\Entity\Status
-         */
-        $severity = ucfirst($status->getSeverity());
-        $code = $status->getCode();
-        $description = $status->getDescription();
-        echo sprintf('%s with code %s and message "%s" occurred.<br>', $severity, $code, $description);
+        // In our example we iterate over all errors and echo them out.
+        // You should display them as error, warning or information based on the given severity.
+        foreach ($response->getStatusCollection() as $status) {
+            /**
+             * @var $status \Wirecard\PaymentSdk\Entity\Status
+             */
+            $severity = ucfirst($status->getSeverity());
+            $code = $status->getCode();
+            $description = $status->getDescription();
+            echo sprintf('%s with code %s and message "%s" occurred.<br>', $severity, $code, $description);
+        }
     }
+// Otherwise a cancel information is printed
+} else {
+    echo 'The transaction has been cancelled.<br>';
 }
--- examples/iDEAL/return.php
+++ examples/iDEAL/return.php
@@ -25,32 +25,38 @@ $config->setPublicKey(file_get_contents(__DIR__ . '/../inc/api-test.wirecard.com
 
 // The `TransactionService` is used to determine the response from the service provider.
 $service = new TransactionService($config);
-$response = $service->handleResponse($_GET);
-
+// If there is response data from the service provider handle response
+if ($_GET) {
+    $response = $service->handleResponse($_GET);
 
 // ## Payment results
 
 // The response from the service can be used for disambiguation.
 // In case of a successful transaction, a `SuccessResponse` object is returned.
-if ($response instanceof SuccessResponse) {
-    echo 'Payment successfully completed.<br>';
-    echo sprintf('Response validation status: %s <br>', $response->isValidSignature() ? 'true' : 'false');
-    echo getTransactionLink($baseUrl, $response);
-    ?>
-    <?php
+    if ($response instanceof SuccessResponse) {
+        echo 'Payment successfully completed.<br>';
+        echo sprintf('Response validation status: %s <br>', $response->isValidSignature() ? 'true' : 'false');
+        echo getTransactionLink($baseUrl, $response);
+        ?>
+        <?php
 // In case of a failed transaction, a `FailureResponse` object is returned.
-} elseif ($response instanceof FailureResponse) {
-    echo sprintf('Response validation status: %s <br>', $response->isValidSignature() ? 'true' : 'false');
+    } elseif ($response instanceof FailureResponse) {
+        echo sprintf('Response validation status: %s <br>', $response->isValidSignature() ? 'true' : 'false');
 
-    // In our example we iterate over all errors and echo them out.
-    // You should display them as error, warning or information based on the given severity.
-    foreach ($response->getStatusCollection() as $status) {
-        /**
-         * @var $status \Wirecard\PaymentSdk\Entity\Status
-         */
-        $severity = ucfirst($status->getSeverity());
-        $code = $status->getCode();
-        $description = $status->getDescription();
-        echo sprintf('%s with code %s and message "%s" occured.<br>', $severity, $code, $description);
+        // In our example we iterate over all errors and echo them out.
+        // You should display them as error, warning or information based on the given severity.
+        foreach ($response->getStatusCollection() as $status) {
+            /**
+             * @var $status \Wirecard\PaymentSdk\Entity\Status
+             */
+            $severity = ucfirst($status->getSeverity());
+            $code = $status->getCode();
+            $description = $status->getDescription();
+            echo sprintf('%s with code %s and message "%s" occured.<br>', $severity, $code, $description);
+        }
     }
+// Otherwise a cancel information is printed
+} else {
+    echo 'The transaction has been cancelled.<br>';
 }
+
