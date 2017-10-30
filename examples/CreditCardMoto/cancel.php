3d4d84e05427c359d245fd3ab1660d47d9ecac86
tomazpu <32056440+tomazpu@users.noreply.github.com>
2017-10-30 14:56:26 +0100

    #237 Credit card moto design update

--- examples/CreditCardMoto/cancel.php
+++ examples/CreditCardMoto/cancel.php
@@ -9,6 +9,7 @@
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../inc/common.php';
 require __DIR__ . '/../inc/config.php';
+//Header design
 require __DIR__ . '/../inc/header.php';
 
 use Wirecard\PaymentSdk\Response\FailureResponse;
@@ -60,5 +61,5 @@ if (!isset($_POST['parentTransactionId'])) {
         }
     }
 }
-
+//Footer design
 require __DIR__ . '/../inc/footer.php';
--- examples/CreditCardMoto/reserve.php
+++ examples/CreditCardMoto/reserve.php
@@ -10,6 +10,7 @@
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../inc/common.php';
 require __DIR__ . '/../inc/config.php';
+//Header design
 require __DIR__ . '/../inc/header.php';
 
 use Wirecard\PaymentSdk\Entity\Amount;
@@ -97,3 +98,5 @@ elseif ($response instanceof FailureResponse):
         echo sprintf('%s with code %s and message "%s" occurred.<br>', $severity, $code, $description);
     }
 endif;
+//Footer design
+require __DIR__ . '/../inc/footer.php';
