6a2eaa92b07dd3a7fcb78d8c9f7703d8e73ca68f
tomazpu <32056440+tomazpu@users.noreply.github.com>
2017-10-30 15:01:25 +0100

    #237 RatePay design update

--- examples/RatePAY_DirectDebit/pay-based-on-reserve.php
+++ examples/RatePAY_DirectDebit/pay-based-on-reserve.php
@@ -9,6 +9,8 @@
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../inc/common.php';
 require __DIR__ . '/../inc/config.php';
+//Header design
+require __DIR__ . '/../inc/header.php';
 
 use Wirecard\PaymentSdk\Entity\Amount;
 use Wirecard\PaymentSdk\Response\FailureResponse;
@@ -102,17 +104,17 @@ if (array_key_exists('item_to_capture', $_POST)) {
     <form action="pay-based-on-reserve.php" method="post">
         <input type="hidden" name="parentTransactionId" value="<?= $parentTransactionId ?>"/>
         <input type="hidden" name="item_to_capture" value="all"/>
-        <input type="submit" value="Capture all items">
+        <button type="submit" class="btn btn-primary">Capture all items</button>
     </form>
     <form action="pay-based-on-reserve.php" method="post">
         <input type="hidden" name="parentTransactionId" value="<?= $parentTransactionId ?>"/>
         <input type="hidden" name="item_to_capture" value="1"/>
-        <input type="submit" value="Capture item 1">
+        <button type="submit" class="btn btn-primary">Capture item 1</button>
     </form>
     <form action="pay-based-on-reserve.php" method="post">
         <input type="hidden" name="parentTransactionId" value="<?= $parentTransactionId ?>"/>
         <input type="hidden" name="item_to_capture" value="2"/>
-        <input type="submit" value="Capture item 2">
+        <button type="submit" class="btn btn-primary">Capture item 2</button>
     </form>
 <?php
 
@@ -134,7 +136,7 @@ if ($response instanceof SuccessResponse) {
             echo sprintf('<input type="hidden" name="amount" value="%0.2f"/>', $amount->getValue());
         }
         ?>
-        <input type="submit" value="Cancel the capture">
+        <button type="submit" class="btn btn-primary">Cancel the capture</button>
     </form>
     <?php
 // In case of a failed transaction, a `FailureResponse` object is returned.
@@ -151,3 +153,5 @@ if ($response instanceof SuccessResponse) {
         echo sprintf('%s with code %s and message "%s" occurred.<br>', $severity, $code, $description);
     }
 }
+//Footer design
+require __DIR__ . '/../inc/footer.php';
--- examples/RatePAY_Installment/cancel.php
+++ examples/RatePAY_Installment/cancel.php
@@ -9,6 +9,8 @@
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../inc/common.php';
 require __DIR__ . '/../inc/config.php';
+//Header design
+require __DIR__ . '/../inc/header.php';
 
 use Wirecard\PaymentSdk\Entity\Amount;
 use Wirecard\PaymentSdk\Response\FailureResponse;
@@ -19,9 +21,11 @@ use Wirecard\PaymentSdk\TransactionService;
 if (!isset($_POST['parentTransactionId'])) {
     ?>
     <form action="cancel.php" method="post">
-        <label for="parentTransactionId">Transaction ID to cancel:</label><br>
-        <input id="parentTransactionId" name="parentTransactionId" style="width:300px"/><br>
-        <input type="submit"/>
+        <div class="form-group">
+            <label for="parentTransactionId">Transaction ID to cancel:</label><br>
+            <input id="parentTransactionId" name="parentTransactionId" class="form-control"/><br>
+        </div>
+        <button type="submit" class="btn btn-primary">Cancel</button>
     </form>
 <?php
 } else {
@@ -88,3 +92,5 @@ if (!isset($_POST['parentTransactionId'])) {
         }
     }
 }
+//Footer design
+require __DIR__ . '/../inc/footer.php';
