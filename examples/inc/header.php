cdaf3142d3680eb1b7f13a38f1acc2b2537c6ce2
tomazpu <32056440+tomazpu@users.noreply.github.com>
2017-10-30 11:00:08 +0100

    #237 header and footer for the examples

---
+++ examples/inc/header.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Wirecard CEE - payment SDK for PHP examples</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="../src/css/example.css" rel="stylesheet">
+</head>
+<body id="overrides">
+<div class="container">
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-2">
+                <img src="../src/img/wirecard_logo.png" alt="wirecard" />
+            </div>
+            <div class="col-sm-10 align-bottom">
+                <h1>Payment SDK for PHP examples</h1>
+            </div>
+        </div>
+    </div>
