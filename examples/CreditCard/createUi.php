b7011e282160decbc22b7d6b577b64a8152419d2
tomazpu <32056440+tomazpu@users.noreply.github.com>
2017-11-03 08:08:27 +0100

    #237 css moved to files

--- examples/CreditCard/createUi.php
+++ examples/CreditCard/createUi.php
@@ -27,7 +27,6 @@ $transactionService = new TransactionService($config);
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" type="application/javascript"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="../src/css/example.css" rel="stylesheet">
     <?php
     // This library is needed to generate the UI and to get a valid token ID.
     ?>
@@ -36,6 +35,62 @@ $transactionService = new TransactionService($config);
         #creditcard-form-div {
             height: 300px;
         }
+
+        #overrides h1 {
+            margin: 0px;
+            font-size: 2vw;
+        }
+
+        #overrides h2 {
+            padding-bottom: 10px;
+            border-bottom: 1px solid #dedede;
+        }
+
+        #overrides h3 {
+            min-height: 52.8px;
+        }
+
+        #overrides img {
+            height: 40px;
+            margin: 0px 20px;
+        }
+
+        #overrides .align-baseline {
+            position: relative;
+        }
+
+        #overrides .bottom-align-text {
+            position: absolute;
+            bottom: -0.35vw;
+            left: 235px;
+        }
+
+        #overrides .page-header {
+            background-color: #002846;
+            margin-top: 0px;
+            padding: 40px 20px;
+            color: white;
+        }
+
+        #overrides .list-group-item {
+            border-radius: 0px;
+            text-transform: uppercase;
+            font-size: 12px;
+        }
+
+        #overrides .list-group-item:hover {
+            color: #ff2014;
+            background-color: #F7F7F8;
+        }
+
+        #overrides .btn-primary {
+            background-color: #002846;
+        }
+
+        #overrides .btn-primary:hover {
+            background-color: #414B56;
+        }
+
     </style>
 </head>
 <body id="overrides">
@@ -43,7 +98,7 @@ $transactionService = new TransactionService($config);
     <div class="page-header">
         <div class="row">
             <div class="col-sm-2">
-                <img src="../src/img/wirecard_logo.png" alt="wirecard" />
+                <img src="https://raw.githubusercontent.com/wirecard/paymentSDK-php/master/examples/src/img/wirecard_logo.png" alt="wirecard" />
             </div>
             <div class="col-sm-10 align-bottom">
                 <h1>Payment SDK for PHP examples</h1>
