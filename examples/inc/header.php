e13e51ef41ab719732349499333dcb86098adc22
jakubpolomsky <jakub.polomsky@partner.wirecard.com>
2017-12-05 17:48:31 +0100

    rename CEE to AG

--- examples/inc/header.php
+++ examples/inc/header.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
-    <title>Wirecard CEE - payment SDK for PHP Examples</title>
+    <title>Wirecard AG - payment SDK for PHP Examples</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <style>
