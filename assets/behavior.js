aa861e30df10ef5a4c1201db3396394eec13a7a2
Travis CI <wirecard@travis-ci.org>
2017-12-12 06:54:30 +0000

    Deploy documentation to GitHub Pages: 1292f63a6f68bc5e3d666e23083710f3f93fcb38

--- assets/behavior.js
+++ assets/behavior.js
@@ -189,8 +189,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                   "type": "heading",
                   "data": {
                     "level": 1,
-                    "title": "Bancontact payment transaction",
-                    "slug": "bancontact-payment-transaction"
+                    "title": "Alipay Crossborder payment transaction",
+                    "slug": "alipay-crossborder-payment-transaction"
                   },
                   "depth": 1,
                   "children": [
