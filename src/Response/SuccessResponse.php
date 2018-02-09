b103893b0f38014e395d961ee1fb2652f0b27af7
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-02-09 12:09:44 +0100

    #268 Prevent possible issues with success transactions

--- src/Response/SuccessResponse.php
+++ src/Response/SuccessResponse.php
@@ -74,7 +74,7 @@ class SuccessResponse extends Response
                 $result = $status['provider-transaction-id'];
             }
 
-            if (strcmp($result, $status['provider-transaction-id']) !== 0) {
+            if (isset($status['provider-transaction-id']) && strcmp($result, $status['provider-transaction-id']) !== 0) {
                 throw new MalformedResponseException('More different provider transaction ID-s in response.');
             }
         }
