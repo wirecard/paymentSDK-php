756af6266763bff005be452e82c956fbd8b683f4
Travis CI <wirecard@travis-ci.org>
2018-06-19 08:57:44 +0000

    Deploy documentation to GitHub Pages: 68098cb0cd3dd0dab1c6749e7b7ee567398eb0d4

--- assets/behavior.js
+++ assets/behavior.js
@@ -7431,6 +7431,73 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                   "foldPrefix": "^",
                   "name": "PHP"
                 },
+                "sourcePath": "/home/travis/build/wirecard/paymentSDK-php/examples/inc/maestroconfig.php",
+                "projectPath": "examples/inc/maestroconfig.php",
+                "targetPath": "examples/inc/maestroconfig",
+                "pageTitle": "examples/inc/maestroconfig",
+                "title": "maestroconfig"
+              },
+              "depth": 3,
+              "outline": [
+                {
+                  "type": "heading",
+                  "data": {
+                    "level": 1,
+                    "title": "Maestro Configuration",
+                    "slug": "maestro-configuration"
+                  },
+                  "depth": 1,
+                  "children": [
+                    {
+                      "type": "heading",
+                      "data": {
+                        "level": 2,
+                        "title": "Required objects",
+                        "slug": "required-objects"
+                      },
+                      "depth": 2
+                    }, {
+                      "type": "heading",
+                      "data": {
+                        "level": 2,
+                        "title": "Connection",
+                        "slug": "connection"
+                      },
+                      "depth": 2
+                    }, {
+                      "type": "heading",
+                      "data": {
+                        "level": 2,
+                        "title": "Payment methods",
+                        "slug": "payment-methods"
+                      },
+                      "depth": 2,
+                      "children": [
+                        {
+                          "type": "heading",
+                          "data": {
+                            "level": 3,
+                            "title": "Maestro",
+                            "slug": "maestro"
+                          },
+                          "depth": 3
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }, {
+              "type": "file",
+              "data": {
+                "language": {
+                  "nameMatchers": [{}, ".fbp"],
+                  "pygmentsLexer": "php",
+                  "singleLineComment": ["//"],
+                  "ignorePrefix": "}",
+                  "foldPrefix": "^",
+                  "name": "PHP"
+                },
                 "sourcePath": "/home/travis/build/wirecard/paymentSDK-php/examples/inc/upiconfig.php",
                 "projectPath": "examples/inc/upiconfig.php",
                 "targetPath": "examples/inc/upiconfig",
