811d2abbea5383150a2584bfe730b83eaf2a6e4d
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-08-17 09:55:05 +0200

    #315 Update tests

--- test/TransactionServiceUTest.php
+++ test/TransactionServiceUTest.php
@@ -45,6 +45,7 @@ use Wirecard\PaymentSdk\Transaction\RatepayDirectDebitTransaction;
 use Wirecard\PaymentSdk\Transaction\RatepayInvoiceTransaction;
 use Wirecard\PaymentSdk\Transaction\RatepayTransaction;
 use Wirecard\PaymentSdk\TransactionService;
+use Mockery as m;
 
 /**
  * Class TransactionServiceUTest
@@ -187,4 +188,18 @@ class TransactionServiceUTest extends \PHPUnit_Framework_TestCase
 
         $this->service->handleResponse(['MD' => 'md', 'PaRes' => 'pares']);
     }
+
+    public function testGetGroupOfTransactions()
+    {
+        $transaction = array(
+            'payment' => array(
+                'payment-method' => 'creditcard'
+            )
+        );
+        $transactionService = m::mock('overload:TransactionService');
+        $transactionService->shouldReceive('getTransactionByTransactionId')->andReturn($transaction);
+
+        $this->assertNotNull($this->service->getGroupOfTransactions('123', 'creditcard'));
+    }
+
 }
