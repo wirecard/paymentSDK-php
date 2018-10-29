733b70669174eefba3e66345af4b932e3c99f005
Tatjana Starcenko <tatjana.starcenko@gmail.com>
2018-10-29 14:43:21 +0100

    #360 Update code to be PSR1/2 compliant

--- tests/_support/Page/CreditCardReservePage.php
+++ tests/_support/Page/CreditCardReservePage.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Shop System Plugins:
+ * - License can be found under:
+ * https://github.com/wirecard/paymentSDK-php/blob/master/LICENSE
+ */
 
 namespace Page;
 
@@ -11,6 +16,12 @@ class CreditCardReservePage
         'Redirect to 3-D Secure page' => "//*[@class='btn btn-primary']"
     );
 
+    /**
+     * Method page element
+     *
+     * @param string $name
+     * @return string
+     */
     public function getElement($name)
     {
         return self::$elements[$name];
