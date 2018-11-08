310a1b7aaaca8dfdb47ce53795cd150ee4f69579
Tatjana Starcenko <tatjana.starcenko@gmail.com>
2018-11-08 14:55:32 +0100

    #379 Change SG instance base URLs

--- examples/inc/config.php
+++ examples/inc/config.php
@@ -60,14 +60,14 @@ $gatewayConfig = function ($key) use ($gateway) {
             'threed_secret' => 'dbc5a498-9a66-43b9-bf1d-a618dd399684'
         ],
         'SECURE-TEST-SG' => [
-            'base_url' => 'https://secure-test.wirecard.com.sg/engine/rest/payments/',
+            'base_url' => 'https://secure-test.wirecard.com.sg',
             'http_user' => 'uatwd_ecom',
             'http_pass' => 'Tomcat123',
             'threed_maid' => 'd7855010-64c1-4e66-9ab3-d98b309a3d8c',
             'threed_secret' => '543d957b-dcc9-46cd-8258-0f49ed97fa8e'
         ],
         'TEST-SG' => [
-            'base_url' => 'https://test.wirecard.com.sg/engine/rest/payments',
+            'base_url' => 'https://test.wirecard.com.sg',
             'http_user' => 'wirecarduser3d',
             'http_pass' => 'Tomcat123',
             'threed_maid' => '961c567b-d9da-41f6-9801-ba21cb228a00',
