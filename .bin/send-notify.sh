fc3a08df37a7581246f81ba89fdfdc89513d10e2
brkicadis <adis.brkic@wirecard.com>
2018-11-12 16:25:46 +0100

    TRAVIS_BUILD_WEB_URL replaced with TRAVIS_JOB_WEB_URL and slack rooms encrypted

--- .bin/send-notify.sh
+++ .bin/send-notify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ "$GATEWAY" = "NOVA" ]]; then
-  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_BUILD_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR\n'}" https://hooks.slack.com/services/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
+  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR\n'}" $NOVA-SLACK
 
 elif [[ "$GATEWAY" = "API-WDCEE-TEST" ]]; then
-  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_BUILD_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR'}" https://hooks.slack.com/services/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
+  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR'}" $API-WDCEE-TEST-SLACK
 
 else
-  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_BUILD_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR'}" https://hooks.slack.com/services/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
+  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR'}" $API-TEST-SLACK
 fi
