8bf8a0a377d6bc044f32cf7f400084e4da40bbd7
Tatjana Starcenko <tatjana.starcenko@gmail.com>
2018-11-13 15:02:00 +0100

    Rename SLACK-ROOMS environment variable to SLACK_ROOMS

--- .bin/send-notify.sh
+++ .bin/send-notify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ "$GATEWAY" = "NOVA" ]]; then
-  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR', 'channel': 'shs-ui-nova'}" $SLACK-ROOMS
+  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR', 'channel': 'shs-ui-nova'}" $SLACK_ROOMS
 
 elif [[ "$GATEWAY" = "API-WDCEE-TEST" ]]; then
-  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR', 'channel': 'shs-ui-api-wdcee-test'}" $SLACK-ROOMS
+  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR', 'channel': 'shs-ui-api-wdcee-test'}" $SLACK_ROOMS
 
 else
-  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR', 'channel': 'shs-ui-api-test'}" $SLACK-ROOMS
+  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR', 'channel': 'shs-ui-api-test'}" $SLACK_ROOMS
 fi
