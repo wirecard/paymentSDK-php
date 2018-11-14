c31941229031758703bfb9ee11d2a2b8263ffd31
Tatjana Starcenko <tatjana.starcenko@gmail.com>
2018-11-14 16:04:26 +0100

    #360 Modify sending notifications and remove unused variables from Travis file

--- .bin/send-notify.sh
+++ .bin/send-notify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ "$GATEWAY" = "NOVA" ]]; then
-  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR', 'channel': 'shs-ui-nova'}" $SLACK_ROOMS
-
+  CHANNEL='shs-ui-nova'
 elif [[ "$GATEWAY" = "API-WDCEE-TEST" ]]; then
-  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR', 'channel': 'shs-ui-api-wdcee-test'}" $SLACK_ROOMS
-
-else
-  curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH\nRepository: $TRAVIS_BUILD_DIR', 'channel': 'shs-ui-api-test'}" $SLACK_ROOMS
+  CHANNEL='shs-ui-api-wdcee-test'
+elif [[ "$GATEWAY" = "API-WDCEE-TEST" ]]; then
+   CHANNEL='shs-ui-api-test'
 fi
+
+curl -X POST -H 'Content-type: application/json' --data "{'text': 'Build Failed. Build URL : $TRAVIS_JOB_WEB_URL\nBuild Number : $TRAVIS_BUILD_NUMBER\nBranch : $TRAVIS_BRANCH', 'channel': '$CHANNEL'}" $SLACK_ROOMS
