9b93d70903a9b90d0468dd40e18a7b1cf7703c77
Tatjana Starcenko <tatjana.starcenko@gmail.com>
2018-11-30 12:10:25 +0100

    #396 Add uploading of the reports also on success

--- .bin/upload-logs-and-notify.sh
+++ .bin/upload-logs-and-notify.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script will upload logs to the report repository
+# And if 'fail' parameter is passed it will send the notification
 set -e # Exit with nonzero exit code if anything fails
 
 export REPO_NAME='reports'
@@ -25,7 +27,9 @@ fi
 
 #copy report files
 cp tests/_output/*.html ${DATE_FOLDER}
-cp tests/_output/*.fail.png ${DATE_FOLDER}
+if [[ $1 == 'fail' ]]; then
+    cp tests/_output/*.fail.png ${DATE_FOLDER}
+fi
 
 cd ${REPO_NAME}
 #push report files to the repository
@@ -35,7 +39,8 @@ git push -q https://${GITHUB_TOKEN}@github.com/wirecard/${REPO_NAME}.git master
 
 #save commit hash
 export SCREENSHOT_COMMIT_HASH=$(git rev-parse --verify HEAD)
-
-#send slack notification
-cd ..
-bash .bin/send-notify.sh
+if [[ $1 == 'fail' ]]; then
+    #send slack notification
+    cd ..
+    bash .bin/send-notify.sh
+fi
