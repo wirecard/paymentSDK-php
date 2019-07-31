5dded1c78d220224c3e0c927b1a037accf21cc2c
rinnhofer <jacqueline.rinnhofer@wirecard.com>
2019-07-31 15:20:40 +0200

    Remove gift card tests

--- test/Entity/RiskInfoUTest.php
+++ test/Entity/RiskInfoUTest.php
@@ -19,30 +19,10 @@ use Wirecard\PaymentSdk\Exception\NotImplementedException;
 
 class RiskInfoUTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSeamlessMappingWithGiftCardCountTooLarge()
-    {
-        $merchantRiskIndicator = new RiskInfo();
-
-        $this->expectException(\InvalidArgumentException::class);
-
-        $merchantRiskIndicator->setGiftCardCount(133);
-    }
-
-    public function testSeamlessMappingWithGiftCardCountTooSmall()
-    {
-        $merchantRiskIndicator = new RiskInfo();
-
-        $this->expectException(\InvalidArgumentException::class);
-
-        $merchantRiskIndicator->setGiftCardCount(0);
-    }
-
     public function testSeamlessMappingWithAllFields()
     {
         $merchantRiskIndicator = new RiskInfo();
-        $giftCardCount         = 13;
         $mail                  = 'max.muster@mail.com';
-        $giftAmount            = new Amount(143.78, 'EUR');
         $date                  = new \DateTime();
 
         $expected = [
@@ -51,16 +31,11 @@ class RiskInfoUTest extends \PHPUnit_Framework_TestCase
             'risk_info_reorder_items'        => RiskInfoReorder::FIRST_TIME_ORDERED,
             'risk_info_availability'         => RiskInfoAvailability::MERCHANDISE_AVAILABLE,
             'risk_info_preorder_date'        => $date->format(RiskInfo::DATE_FORMAT),
-            'risk_info_gift_amount'          => 143,
-            'risk_info_gift_amount_currency' => $giftAmount->getCurrency(),
-            'risk_info_gift_card_count'      => $giftCardCount,
         ];
 
         $merchantRiskIndicator->setAvailability(RiskInfoAvailability::MERCHANDISE_AVAILABLE);
         $merchantRiskIndicator->setDeliveryEmailAddress($mail);
         $merchantRiskIndicator->setDeliveryTimeFrame(RiskInfoDeliveryTimeFrame::ELECTRONIC_DELIVERY);
-        $merchantRiskIndicator->setGiftAmount($giftAmount);
-        $merchantRiskIndicator->setGiftCardCount($giftCardCount);
         $merchantRiskIndicator->setPreOrderDate($date);
         $merchantRiskIndicator->setReorderItems(RiskInfoReorder::FIRST_TIME_ORDERED);
 
