6829ae96359b1e65ef081b767d8148c2941dbc0f
brkicadis <adis.brkic@wirecard.com>
2019-08-07 14:54:37 +0200

    Change amount for non3ds tests

--- examples/payments/CreditCard/createUi.php
+++ examples/payments/CreditCard/createUi.php
@@ -48,7 +48,7 @@ if (strpos($gatewayEnv, 'SG') !== false) {
 }
 
 if (isset($_GET['amount'])) {
-    $startAmount = 70;
+    $paymentAction = $_GET['paymentAction'];
 }
 
 $amount = new Amount($startAmount, $currency);
