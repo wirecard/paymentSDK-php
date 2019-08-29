68d276a2306970d7706e512171913051ef956209
ozada <oskar.huseyn@wirecard.com>
2019-08-29 11:36:27 +0200

    Improve getEndpoint method of PayByBankAppTransaction class.

--- src/Transaction/PayByBankAppTransaction.php
+++ src/Transaction/PayByBankAppTransaction.php
@@ -9,6 +9,8 @@
 
 namespace Wirecard\PaymentSdk\Transaction;
 
+use Wirecard\PaymentSdk\Exception\MandatoryFieldMissingException;
+
 class PayByBankAppTransaction extends Transaction
 {
     const NAME = 'zapp';
@@ -21,11 +23,18 @@ class PayByBankAppTransaction extends Transaction
         return array();
     }
 
+    /**
+     * @return string
+     */
     protected function retrieveTransactionTypeForPay()
     {
         return self::TYPE_DEBIT;
     }
 
+    /**
+     * @return string
+     * @throws MandatoryFieldMissingException
+     */
     protected function retrieveTransactionTypeForCancel()
     {
         if (!$this->parentTransactionId) {
@@ -34,4 +43,17 @@ class PayByBankAppTransaction extends Transaction
 
         return Transaction::TYPE_REFUND_REQUEST;
     }
+
+    /**
+     * return string
+     */
+    public function getEndpoint()
+    {
+        if (in_array($this->operation, [Operation::CANCEL, Operation::REFUND])) {
+            return self::ENDPOINT_PAYMENTS;
+        }
+
+        return self::ENDPOINT_PAYMENT_METHODS;
+    }
+
 }
