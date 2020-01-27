6ffef93a39f80c90af806f3ec659e140d8cbc750
Sebastijan Sakac <sebastijan.sakac@wirecard.com>
2020-01-27 12:10:30 +0100

    Refactor SeamlessMapper UnitTest

--- test/Mapper/Response/SeamlessMapperUTest.php
+++ test/Mapper/Response/SeamlessMapperUTest.php
@@ -20,6 +20,7 @@ class SeamlessMapperUTest extends PHPUnit_Framework_TestCase
     const EXPECTED_CARD_TYPE = 'visa';
     const EXPECTED_EXPIRATION_YEAR = 2023;
     const EXPECTED_EXPIRATION_MONTH = 1;
+    const EXPECTED_NULL = null;
 
     const FIELD_CARD_TYPE = 'card-type';
     const FIELD_EXPIRATION_YEAR = 'expiration-year';
@@ -30,131 +31,56 @@ class SeamlessMapperUTest extends PHPUnit_Framework_TestCase
     const PAYLOAD_FIELD_EXPIRATION_MONTH = 'expiration_month';
 
     /**
-     * Test optional credit card data (card type, expiration year and expiration month)
+     * @dataProvider optionalCreditCardDataProvider
+     * @param $payloadData
+     * @param $expected
+     * @param $expectedType
      */
-    public function testAllOptionalCreditCardData()
+    public function testOptionalCreditCardData($payloadData, $expected, $expectedType)
     {
-        $result = $this->getPayloadResult($this->payloadData());
-        $this->assertEquals(self::EXPECTED_CARD_TYPE, $this->getCardType($result));
-        $this->assertEquals(self::EXPECTED_EXPIRATION_YEAR, $this->getExpirationYear($result));
-        $this->assertEquals(self::EXPECTED_EXPIRATION_MONTH, $this->getExpirationMonth($result));
+        $current = $this->getResultField($expectedType, $payloadData);
+        $this->assertEquals($expected, $current);
     }
 
     /**
-     * Test optional credit card data if expiration year is not included
+     * @return array
      */
-    public function testOptionalCreditCardDataWithoutYear()
+    public function optionalCreditCardDataProvider()
     {
-        $result = $this->getPayloadResult($this->getPayloadWithoutYear());
-        $this->assertEquals(self::EXPECTED_CARD_TYPE, $this->getCardType($result));
-        $this->assertNull($this->getExpirationYear($result));
-        $this->assertNull($this->getExpirationMonth($result));
-    }
+        return [
+            [$this->payloadData(), self::EXPECTED_CARD_TYPE, self::FIELD_CARD_TYPE],
+            [$this->payloadData(), self::EXPECTED_EXPIRATION_YEAR, self::FIELD_EXPIRATION_YEAR],
+            [$this->payloadData(), self::EXPECTED_EXPIRATION_MONTH, self::FIELD_EXPIRATION_MONTH],
 
-    /**
-     * Test optional credit card data if expiration month is not included
-     */
-    public function testOptionalCreditCardDataWithoutMonth()
-    {
-        $result = $this->getPayloadResult($this->getPayloadWithoutMonth());
-        $this->assertEquals(self::EXPECTED_CARD_TYPE, $this->getCardType($result));
-        $this->assertNull($this->getExpirationYear($result));
-        $this->assertNull($this->getExpirationMonth($result));
-    }
+            [$this->getPayloadWithoutYear(), self::EXPECTED_CARD_TYPE, self::FIELD_CARD_TYPE],
+            [$this->getPayloadWithoutYear(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_YEAR],
+            [$this->getPayloadWithoutYear(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_MONTH],
 
-    /**
-     * Test optional credit card data if expiration year is not included
-     */
-    public function testOptionalCreditCardDataWithoutMonthAndYear()
-    {
-        $result = $this->getPayloadResult($this->getPayloadWithoutMonthAndYear());
-        $this->assertEquals(self::EXPECTED_CARD_TYPE, $this->getCardType($result));
-        $this->assertNull($this->getExpirationYear($result));
-        $this->assertNull($this->getExpirationMonth($result));
-    }
+            [$this->getPayloadWithoutMonth(), self::EXPECTED_CARD_TYPE, self::FIELD_CARD_TYPE],
+            [$this->getPayloadWithoutMonth(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_YEAR],
+            [$this->getPayloadWithoutMonth(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_MONTH],
 
-    /**
-     * Test optional credit card data if card type is not included
-     */
-    public function testOptionalCreditCardDataWithoutCardType()
-    {
-        $result = $this->getPayloadResult($this->getPayloadWithoutCardType());
-        $this->assertNull($this->getCardType($result));
-        $this->assertEquals(self::EXPECTED_EXPIRATION_YEAR, $this->getExpirationYear($result));
-        $this->assertEquals(self::EXPECTED_EXPIRATION_MONTH, $this->getExpirationMonth($result));
-    }
+            [$this->getPayloadWithoutMonthAndYear(), self::EXPECTED_CARD_TYPE, self::FIELD_CARD_TYPE],
+            [$this->getPayloadWithoutMonthAndYear(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_YEAR],
+            [$this->getPayloadWithoutMonthAndYear(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_MONTH],
 
-    /**
-     * Test optional credit card data if expiration year and card type are not included
-     */
-    public function testOptionalCreditCardDataWithoutYearAndCardType()
-    {
-        $result = $this->getPayloadResult($this->getPayloadWithoutYearAndCardType());
-        $this->assertNull($this->getCardType($result));
-        $this->assertNull($this->getExpirationYear($result));
-        $this->assertNull($this->getExpirationMonth($result));
-    }
+            [$this->getPayloadWithoutCardType(), self::EXPECTED_NULL, self::FIELD_CARD_TYPE],
+            [$this->getPayloadWithoutCardType(), self::EXPECTED_EXPIRATION_YEAR, self::FIELD_EXPIRATION_YEAR],
+            [$this->getPayloadWithoutCardType(), self::EXPECTED_EXPIRATION_MONTH, self::FIELD_EXPIRATION_MONTH],
 
-    /**
-     * Test optional credit card data if expiration month and card type are not included
-     */
-    public function testOptionalCreditCardDataWithoutMonthAndCardType()
-    {
-        $result = $this->getPayloadResult($this->getPayloadWithoutMonthAndCardType());
-        $this->assertNull($this->getCardType($result));
-        $this->assertNull($this->getExpirationYear($result));
-        $this->assertNull($this->getExpirationMonth($result));
-    }
+            [$this->getPayloadWithoutYearAndCardType(), self::EXPECTED_NULL, self::FIELD_CARD_TYPE],
+            [$this->getPayloadWithoutYearAndCardType(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_YEAR],
+            [$this->getPayloadWithoutYearAndCardType(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_MONTH],
 
-    /**
-     * Test optional credit card data if optional data is not included
-     */
-    public function testOptionalCreditCardDataWithoutOptionalData()
-    {
-        $result = $this->getPayloadResult($this->getPayloadWithoutOptionalData());
-        $this->assertNull($this->getCardType($result));
-        $this->assertNull($this->getExpirationYear($result));
-        $this->assertNull($this->getExpirationMonth($result));
-    }
+            [$this->getPayloadWithoutMonthAndCardType(), self::EXPECTED_NULL, self::FIELD_CARD_TYPE],
+            [$this->getPayloadWithoutMonthAndCardType(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_YEAR],
+            [$this->getPayloadWithoutMonthAndCardType(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_MONTH],
 
-    /**
-     * @param array $payload
-     * @return FailureResponse|FormInteractionResponse|SuccessResponse
-     */
-    private function getPayloadResult(array $payload)
-    {
-        $seamlessMapper = new SeamlessMapper($payload);
-        /** @var FormInteractionResponse $result */
-        return $seamlessMapper->map();
+            [$this->getPayloadWithoutOptionalData(), self::EXPECTED_NULL, self::FIELD_CARD_TYPE],
+            [$this->getPayloadWithoutOptionalData(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_YEAR],
+            [$this->getPayloadWithoutOptionalData(), self::EXPECTED_NULL, self::FIELD_EXPIRATION_MONTH],
+        ];
     }
-
-    /**
-     * @param FormInteractionResponse $result
-     * @return mixed|null
-     */
-    private function getCardType(FormInteractionResponse $result)
-    {
-        return $this->getResultField(self::FIELD_CARD_TYPE, $result);
-    }
-
-    /**
-     * @param FormInteractionResponse $result
-     * @return mixed|null
-     */
-    private function getExpirationYear(FormInteractionResponse $result)
-    {
-        return $this->getResultField(self::FIELD_EXPIRATION_YEAR, $result);
-    }
-
-    /**
-     * @param FormInteractionResponse $result
-     * @return mixed|null
-     */
-    private function getExpirationMonth(FormInteractionResponse $result)
-    {
-        return $this->getResultField(self::FIELD_EXPIRATION_MONTH, $result);
-    }
-
     /**
      * @param $name
      * @param FormInteractionResponse $result
@@ -173,84 +99,103 @@ class SeamlessMapperUTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * @return array
+     * @return FailureResponse|FormInteractionResponse|SuccessResponse
      */
     private function getPayloadWithoutYear()
     {
-        $data = $this->payloadData();
-        unset($data[self::PAYLOAD_FIELD_EXPIRATION_YEAR]);
-        return $data;
+        $payload = $this->payloadArray();
+        unset($payload[self::PAYLOAD_FIELD_EXPIRATION_YEAR]);
+        return $this->getSeamlessMapper($payload);
     }
 
     /**
-     * @return array
+     * @return FailureResponse|FormInteractionResponse|SuccessResponse
      */
     private function getPayloadWithoutMonth()
     {
-        $data = $this->payloadData();
-        unset($data[self::PAYLOAD_FIELD_EXPIRATION_MONTH]);
-        return $data;
+        $payload = $this->payloadArray();
+        unset($payload[self::PAYLOAD_FIELD_EXPIRATION_MONTH]);
+        return $this->getSeamlessMapper($payload);
     }
 
     /**
-     * @return array
+     * @return FailureResponse|FormInteractionResponse|SuccessResponse
      */
     private function getPayloadWithoutMonthAndYear()
     {
-        $data = $this->payloadData();
-        unset($data[self::PAYLOAD_FIELD_EXPIRATION_YEAR]);
-        unset($data[self::PAYLOAD_FIELD_EXPIRATION_MONTH]);
-        return $data;
+        $payload = $this->payloadArray();
+        unset($payload[self::PAYLOAD_FIELD_EXPIRATION_YEAR]);
+        unset($payload[self::PAYLOAD_FIELD_EXPIRATION_MONTH]);
+        return $this->getSeamlessMapper($payload);
     }
 
     /**
-     * @return array
+     * @return FailureResponse|FormInteractionResponse|SuccessResponse
      */
     private function getPayloadWithoutCardType()
     {
-        $data = $this->payloadData();
-        unset($data[self::PAYLOAD_FIELD_CARD_TYPE]);
-        return $data;
+        $payload = $this->payloadArray();
+        unset($payload[self::PAYLOAD_FIELD_CARD_TYPE]);
+        return $this->getSeamlessMapper($payload);
     }
 
     /**
-     * @return array
+     * @return FailureResponse|FormInteractionResponse|SuccessResponse
      */
     private function getPayloadWithoutYearAndCardType()
     {
-        $data = $this->payloadData();
-        unset($data[self::PAYLOAD_FIELD_CARD_TYPE]);
-        unset($data[self::PAYLOAD_FIELD_EXPIRATION_YEAR]);
-        return $data;
+        $payload = $this->payloadArray();
+        unset($payload[self::PAYLOAD_FIELD_CARD_TYPE]);
+        unset($payload[self::PAYLOAD_FIELD_EXPIRATION_YEAR]);
+        return $this->getSeamlessMapper($payload);
     }
 
     /**
-     * @return array
+     * @return FailureResponse|FormInteractionResponse|SuccessResponse
      */
     private function getPayloadWithoutMonthAndCardType()
     {
-        $data = $this->payloadData();
-        unset($data[self::PAYLOAD_FIELD_CARD_TYPE]);
-        unset($data[self::PAYLOAD_FIELD_EXPIRATION_MONTH]);
-        return $data;
+        $payload = $this->payloadArray();
+        unset($payload[self::PAYLOAD_FIELD_CARD_TYPE]);
+        unset($payload[self::PAYLOAD_FIELD_EXPIRATION_MONTH]);
+        return $this->getSeamlessMapper($payload);
     }
 
     /**
-     * @return array
+     * @return FailureResponse|FormInteractionResponse|SuccessResponse
      */
     private function getPayloadWithoutOptionalData()
     {
-        $data = $this->payloadData();
-        unset($data[self::PAYLOAD_FIELD_CARD_TYPE]);
-        unset($data[self::PAYLOAD_FIELD_EXPIRATION_YEAR]);
-        unset($data[self::PAYLOAD_FIELD_EXPIRATION_MONTH]);
-        return $data;
+        $payload = $this->payloadArray();
+        unset($payload[self::PAYLOAD_FIELD_CARD_TYPE]);
+        unset($payload[self::PAYLOAD_FIELD_EXPIRATION_YEAR]);
+        unset($payload[self::PAYLOAD_FIELD_EXPIRATION_MONTH]);
+        return $this->getSeamlessMapper($payload);
+    }
+
+    /**
+     * @return FailureResponse|FormInteractionResponse|SuccessResponse
+     */
+    private function payloadData()
+    {
+        return $this->getSeamlessMapper($this->payloadArray());
+    }
+
+    /**
+     * @param $payload
+     * @return FailureResponse|FormInteractionResponse|SuccessResponse
+     */
+    private function getSeamlessMapper($payload)
+    {
+        $seamlessMapper = new SeamlessMapper($payload);
+        /** @var FormInteractionResponse $result */
+        return $seamlessMapper->map();
     }
 
     /**
      * @return array
      */
-    private function payloadData()
+    private function payloadArray()
     {
         $json = json_decode($this->payloadJsonString(), true);
         return $json['payload'];
@@ -264,10 +209,17 @@ class SeamlessMapperUTest extends PHPUnit_Framework_TestCase
         return '{
         "payload" : {
             "country" : "AT",
-            "notification_url_1" : "https://api-wdcee-test.wirecard.com:443/engine/rest/hpp/acs/e580a945-d353-4641-9683-b778b7cacb5f/",
+            "notification_url_1" : "https://api-wdcee-test.wirecard.com:443' .
+            '/engine/rest/hpp/acs/e580a945-d353-4641-9683-b778b7cacb5f/",
             "response_signature" : "9a5856505da1efc86e50f56cafcdcd6b81b4ee2df845b86c742d1b33862958da",
             "provider_status_code_1" : "0",
-            "response_signature_v2" : "SFMyNTYKdHJhbnNhY3Rpb25faWQ9ZTU4MGE5NDUtZDM1My00NjQxLTk2ODMtYjc3OGI3Y2FjYjVmCmNvbXBsZXRpb25fdGltZXN0YW1wPTIwMjAwMTIxMTE1MjA3Cm1hc2tlZF9hY2NvdW50X251bWJlcj00MDEyMDAqKioqKioxMDAzCnRva2VuX2lkPTQzOTc0MjM0NzYxMDEwMDMKYXV0aG9yaXphdGlvbl9jb2RlPQptZXJjaGFudF9hY2NvdW50X2lkPTQ5ZWUxMzU1LWNkZDMtNDIwNS05MjBmLTg1MzkxYmIzODY1ZAp0cmFuc2FjdGlvbl9zdGF0ZT1zdWNjZXNzCmlwX2FkZHJlc3M9MTcyLjIyLjAuMQp0cmFuc2FjdGlvbl90eXBlPXB1cmNoYXNlCnJlcXVlc3RfaWQ9MzAzYmE3OGZiZDVkNmNjMzNhNzMxMWFlNDg1ODYyZDc4OTAxNzg5YWE1ZTM3ZWJlM2YyNzI5YzljNGE1NTAxYwo=.zgD/W3kwQRSrkOeWAIedKIYvuv5Z7hI61gdJIxQjq/o=",
+            "response_signature_v2" : "SFMyNTYKdHJhbnNhY3Rpb25faWQ9ZTU4MGE5NDUtZDM1My00NjQxLTk2ODMtYj' .
+            'c3OGI3Y2FjYjVmCmNvbXBsZXRpb25fdGltZXN0YW1wPTIwMjAwMTIxMTE1MjA3Cm1hc2tlZF9hY2NvdW50X251bW' .
+            'Jlcj00MDEyMDAqKioqKioxMDAzCnRva2VuX2lkPTQzOTc0MjM0NzYxMDEwMDMKYXV0aG9yaXphdGlvbl9jb2RlPQ' .
+            'ptZXJjaGFudF9hY2NvdW50X2lkPTQ5ZWUxMzU1LWNkZDMtNDIwNS05MjBmLTg1MzkxYmIzODY1ZAp0cmFuc2FjdG' .
+            'lvbl9zdGF0ZT1zdWNjZXNzCmlwX2FkZHJlc3M9MTcyLjIyLjAuMQp0cmFuc2FjdGlvbl90eXBlPXB1cmNoYXNlCn' .
+            'JlcXVlc3RfaWQ9MzAzYmE3OGZiZDVkNmNjMzNhNzMxMWFlNDg1ODYyZDc4OTAxNzg5YWE1ZTM3ZWJlM2YyNzI5Yz' .
+            'ljNGE1NTAxYwo=.zgD/W3kwQRSrkOeWAIedKIYvuv5Z7hI61gdJIxQjq/o=",
             "provider_status_code_2" : "0",
             "browser_hostname" : "10.137.8.253",
             "provider_status_description_2" : "Transaction OK",
@@ -285,12 +237,20 @@ class SeamlessMapperUTest extends PHPUnit_Framework_TestCase
             "status_code_2" : "200.1077",
             "status_description_1" : "3d-acquirer:The resource was successfully created.",
             "phone" : "123123123",
-            "status_description_2" : "3d-acquirer:Card is eligible for the ACS authentication process. If the customer has not yet activated his card for 3-D Secure processing his issuer may offer activation during shopping.",
+            "status_description_2" : "3d-acquirer:Card is eligible for the ACS authentication process. ' .
+            'If the customer has not yet activated his card for 3-D Secure processing his issuer may ' .
+            'offer activation during shopping.",
             "parent_transaction_id" : "d6de377c-4382-465a-b728-cef6bd89e0ef",
             "city" : "city",
             "requested_amount" : "104.4",
             "nonce3d" : "9A43BD599FD6585D886B1F084E20CDBC",
-            "pareq" : "eJxtU8tuo0AQvOcrEPdlHgwva5goWctZH7JKNjEr+YaGViAy4AwQ23+/DbaTtQEJie6uoatrquXtvtxYn2Caoq5imznUtqDSdVZUb7G9el38CG2radMqSzd1BbF9gMa+VTfyNTcA8xfQnQF1Y+EjH6Fp0jewiiy2hRcx1/coDRkXQtDIj3z7iBuwT3d/4OM7HnInFgpJOFySc3gJegSj87RqL9NDKdUf98vfinFXeL4kp3CMK8Es5wq5UYYvleSYGAOrtAT1tzCgU5NZcyhr6yWvt5IMhTFe113VmoMKObY/B2NYZzYqb9vtjJDdbufsTg0cXZeS9MXLicn0yPKp69PNFJF9kSldJp/ZQ9KtkvU6fVgvE7rwk0W+WL0/x5L0iPG5LG1BccpRGc4sxmcen1GcZchPCF72MypGhSNQ72M0hm1PRO++4AJFv8pOqNkZg1Y8qCgIUc5zNAbCfovWxH+gab6+rzScFkv+/DXpI92iH6jLIsGYH4Su5wYeD33hBy4aOqRB764BNMmmwGvnLmUDneLaA5L83xWpfa9Cf9fDDuGCkcsN+wcH3ugm",
+            "pareq" : "eJxtU8tuo0AQvOcrEPdlHgwva5goWctZH7JKNjEr+YaGViAy4AwQ23+/DbaTtQEJie6uoatrquXtvtx' .
+            'Yn2Caoq5imznUtqDSdVZUb7G9el38CG2radMqSzd1BbF9gMa+VTfyNTcA8xfQnQF1Y+EjH6Fp0jewiiy2hRcx1/co' .
+            'DRkXQtDIj3z7iBuwT3d/4OM7HnInFgpJOFySc3gJegSj87RqL9NDKdUf98vfinFXeL4kp3CMK8Es5wq5UYYvleSYG' .
+            'AOrtAT1tzCgU5NZcyhr6yWvt5IMhTFe113VmoMKObY/B2NYZzYqb9vtjJDdbufsTg0cXZeS9MXLicn0yPKp69PNFJ' .
+            'F9kSldJp/ZQ9KtkvU6fVgvE7rwk0W+WL0/x5L0iPG5LG1BccpRGc4sxmcen1GcZchPCF72MypGhSNQ72M0hm1PRO+' .
+            '+4AJFv8pOqNkZg1Y8qCgIUc5zNAbCfovWxH+gab6+rzScFkv+/DXpI92iH6jLIsGYH4Su5wYeD33hBy4aOqRB764B' .
+            'NMmmwGvnLmUDneLaA5L83xWpfa9Cf9fDDuGCkcsN+wcH3ugm",
             "merchant_account_id" : "49ee1355-cdd3-4205-920f-85391bb3865d",
             "expiration_month" : "1",
             "street1" : "addtess 12",
